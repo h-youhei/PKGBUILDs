e26e2410cade73ad8020a11a93f53ba434cc6957
h-youhei <hukumitu.youhei@gmail.com>
2018-03-13 12:36:37 +0900

    add slock

---
+++ abs/slock/config.h
@@ -0,0 +1,12 @@
+/* user and group to drop privileges to */
+static const char *user  = "nobody";
+static const char *group = "nobody";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#002200",   /* during input */
+	[FAILED] = "#550000",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 0;
